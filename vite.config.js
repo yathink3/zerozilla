ffdc35f0465e21bce095eea82cf775e9aec294b7
yathin <yathink3@gmail.com>
2021-10-06 22:17:05 +0530

    pwa settings updated

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,9 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
-import reactSvgPlugin from "vite-plugin-react-svg";
+import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+import reactSvgPlugin from 'vite-plugin-react-svg';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), reactSvgPlugin()],
+  plugins: [react(), reactSvgPlugin(), VitePWA()],
 });
